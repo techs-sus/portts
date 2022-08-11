25158102e5e0c867ab0f6e5764e0f019899088fc
techs-sus <92276908+techs-sus@users.noreply.github.com>
2022-08-11 09:45:55 -0400

    dev: commit

--- index.js
+++ index.js
@@ -8,24 +8,10 @@ import fs from "fs/promises";
 import path from "path";
 import { v4 } from "uuid";
 import { fileURLToPath } from "url";
-
 const app = express();
-const port = process.env.PORT || 6969;
-const regex = /-- @inject.+-- @end_inject/gms;
+const port = process.env.PORT || 3000;
+const regex = /TS\.import\(script, ?script, ?['"]([a-zA-Z0-9_]+)['"]\)/gm
 
-app.use(express.json());
-
-app.use("/npm", express.static("node_modules"));
-app.use("/src", express.static("out"));
-app.use("/lib", express.static("include/RuntimeLib.lua"));
-
-createTunnel({ port: port, subdomain: v4() }).then(async (tun) => {
-	app.listen(port);
-	console.log(`${tun.url} (copied to clipboard)`);
-	const code = await fs.readFile("include/RuntimeLib.lua");
-	await fs.writeFile(
-		"include/RuntimeLib.lua",
-		code.toString().replace(regex, `-- @inject\nlocal url = "${tun.url}"\n-- @end_inject`),
-	);
-	clipboard.write("h/" + tun.url + "/lib").catch((e) => {});
-});
+async function link(file) {
+  const contents = await fs.readFile(file)
+}
